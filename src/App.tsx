3314a54e1a356dedd3db4cf1940c795772277acf
e.shamray <e.shamray@ossystem.ua>
2021-06-04 16:43:10 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Cells from 'components/cells';
+
+function App() {
+  return (
+    <div className="App">
+      <Cells />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/cells/cells.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './cells.css';
+
+function Cells() {
+  return (
+    <div className="cells">
+      Cells
+    </div>
+  );
+}
+
+export default Cells;
